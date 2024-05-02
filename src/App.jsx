2ebc635140af2b71a7aff748b4a82cf2bd5c374c
dimebruce <127O1404@itsx.edu.mx>
2024-05-02 13:53:51 -0600

    Start project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Remember me</h1>
+      <div className="video-container">
+        <video controls autoPlay loop className="responsive-video">
+          <source src="/video/1.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </>
+  )
+}
+
+export default App
