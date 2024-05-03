73e0a271c0aea5a65706633e93870d3bdb542ec5
dimebruce <127O1404@itsx.edu.mx>
2024-05-03 16:06:30 -0600

    cchange video

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Remember me</h1>
       <div className="video-container">
         <video controls autoPlay loop className="responsive-video" controlsList="nodownload" disablePictureInPicture>
-          <source src="/video/1.mp4" type="video/mp4" />
+          <source src="https://melodious-cendol-3a7b9d.netlify.app/cheque2a.mp4" type="video/mp4" />
         </video>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
